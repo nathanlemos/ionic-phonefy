2c506233fe265e3bdfc1fe89673fdee50d4f2296
nathanlemos <nathanlemos.ec@gmail.com>
2018-11-13 13:38:13 -0200

    Update phonefy.ts

--- phonefy.ts
+++ phonefy.ts
@@ -8,7 +8,7 @@ export class PhonefyPipe implements PipeTransform
 {
 	transform(str: string): string
 	{
-		return str.replace(/[\s]\+?([0-9]{2})?\s?([0-9]{2})?\s?((?:\()[0-9]{2}(?:\)))?\s?[0-9]{4,5}(?:-?)[0-9]{4}/mg, function(v1)
+		return str.replace(/[\s]?\+?([0-9]{2})?\s?([0-9]{2})?\s?((?:\()[0-9]{2}(?:\)))?\s?[0-9]{4,5}(?:-?)[0-9]{4}/mg, function(v1)
 			{ 
 				let link  = v1.replace(" ", "").replace("(", "").replace(")", "").replace("-", "");
 				let texto = v1.replace(" ", "");
